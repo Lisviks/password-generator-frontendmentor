fa500a9d07e19bf4b6167ad4066793a1f9001d94
Deividas Rimkus <8516648+Lisviks@users.noreply.github.com>
2023-06-12 19:54:15 +0100

    copy password to clipboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,12 +7,16 @@ import { useState } from 'react';
 export default function Home() {
   const [password, setPassword] = useState('');
 
+  const handleCopy = () => {
+    if (password) navigator.clipboard.writeText(password);
+  };
+
   return (
     <main>
       <h1>Password Generator</h1>
       <div className='result'>
         <input className='password' placeholder='P4$5W0rD!' value={password} disabled />
-        <div className='copy'>
+        <div className='copy' onClick={handleCopy}>
           <IconCopy />
         </div>
       </div>
